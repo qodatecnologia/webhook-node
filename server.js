0bdc7f7424c94bb25b72c2e8e902adcf963e0b33
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-09 20:53:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// server.js
+// where your node app starts
+
+// init project
 const express = require('express');
 const app = express();
 
@@ -11,51 +15,37 @@ app.use(bodyParser.urlencoded({
 //mysql para conexão com database
 const mysql = require("mysql");
 
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 //entrada no database
 app.post('/cadastrados', function(request, response) {
 
   var connection = mysql.createConnection({
-      host     : process.env.MYSQL_HOST,
-      user     : process.env.MYSQL_USER,
-      password : process.env.MYSQL_PASS,
-      database : process.env.MYSQL_DB  
+      host     : process.env.HOST,
+      user     : process.env.USER,
+      password : process.env.PASS,
+      database : process.env.DB  
   });
   connection.connect();
-});  
-  // instancia intent
-  var intentName = request.body.queryResult.intent.displayName;
   
-  if (intentName == 'cadastro') {
-    console.log('cadastrar');  
-    
+var intentName = request.body.queryResult.intent.displayName;
+
+ if (intentName == 'cadastro') {
     var nome_ = request.body.queryResult.parameters.nome;
     var email_   = request.body.queryResult.parameters.email;
-    
     var query = 'insert into cadastrados values ("'+nome_+'","'+email_+'")';
-    
-    connection.query(query, function (error, results, fields) {
-       if (error) throw error;
-       connection.end();
-       response.json({"fulfillmentText" :"Ok, cadastro realizado" });
-    });   
-  } 
-
-
+ };
 // listen for requests :)
-//const listener = app.listen(process.env.PORT, function() {
-  //console.log('Your app is listening on port ' + listener.address().port);
-//});
-app.listen(process.env.PORT);
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+const listener = app.listen(process.env.PORT, function() {
+ console.log(listener.address().port);
+});
+});    
