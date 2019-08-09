2d63f95025c2e109a64ccaa92b467a58e99a8285
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-09 11:48:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/cadastrados', function(request, response) {
       database : process.env.MYSQL_DB  
   });
   connection.connect();
-};  
+});  
   // instancia intent
   var intentName = request.body.queryResult.intent.displayName;
   
