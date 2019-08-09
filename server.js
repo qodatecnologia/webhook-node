dd6a5f1b9b883b72b4950127c7cdb6f546a1b425
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-09 16:00:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var http = require('http');
+var request = require('http');
 
 // instancia body parser para suportar json
 const bodyParser = require('body-parser');
